6c0fc414afc43b84a1ad10b8995b29e906faf601
Tiago Martins <grupomartinscomunicacao@gmail.com>
2026-01-27 10:25:33 -0300

    Fix: Sitemaps modificados

--- apps/blog/sitemaps.py
+++ apps/blog/sitemaps.py
@@ -24,7 +24,6 @@ class StaticViewSitemap(Sitemap):
             'endereco_virtual',
             'certificado_digital',
             'emissor_nota_fiscal',
-            'contabilidade_online',
         ]
 
     def location(self, item):
